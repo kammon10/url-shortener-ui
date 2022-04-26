65007ad51245b3188b51ab66b688c591c044aab0
Katie Ammon <katie.ammon10@gmail.com>
2022-04-26 10:33:20 -0600

    Finish iteration 4 testing

---
+++ cypress/integration/User-flow-iteration-4.js
@@ -0,0 +1,26 @@
+describe('User flow for iteration 3', () => {
+     let testData;
+
+     beforeEach(() => {
+          testData = {
+               "urls": [
+{
+"id": 1,
+"long_url": "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+"short_url": "http://localhost:3001/useshorturl/1",
+"title": "Awesome photo"
+} ]
+          }
+     })
+
+     it('When a user fills out and submits the form, the new shortened URL is rendered', () => {
+          cy.visit('http://localhost:3000/')
+          cy.intercept('http://localhost:3001/api/v1/urls', testData)
+          .get('.title-input').type('Awesome photo')
+          .get('.longUrl-input').type("https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80")
+          .get('.submit').click()
+          .get('.url').contains('http://localhost:3001/useshorturl/1')
+     })
+
+     
+})
